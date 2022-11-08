b65b84809a8b4def72edb5dc21d7861f271bd827
sainath11-reddy <sainath11reddy@gmail.com>
2022-11-08 18:37:25 +0530

    deploying the frontend

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 var cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path')
 dotenv.config();
 
 const bodyParser = require('body-parser');
@@ -12,9 +13,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json({extended:false})); 
 app.use('/admin',routes);
-app.use('/',(req, res, next)=>{
+app.use((req, res, next)=>{
     
-    res.send("<h1>Active</h1>");
+    res.sendFile(path.join(__dirname,`public/${req.url}`));
 } );
 sequelize.sync().then(resolve =>{   
     console.log("Server synced");
